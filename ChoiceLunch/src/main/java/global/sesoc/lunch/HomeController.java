504800a7fac3802ba305eff7696d961f43f24060
BGM <1lsummer99@gmail.com>
2018-08-12 16:58:29 +0900

    커밋 다시 테스트

--- ChoiceLunch/src/main/java/global/sesoc/lunch/HomeController.java
+++ ChoiceLunch/src/main/java/global/sesoc/lunch/HomeController.java
@@ -33,12 +33,12 @@ public class HomeController {
 	
 	public String TestMethod() {
 	System.out.println("테스트 메소드");
-		return "2";
+		return "11";
 	}
 	
 
 	public String TestMethod2() {
-		return "1";
+		return "22";
 	}
 	
 
